bbafc3c6395d706af2a5158cbe9a6265e3e6a60c
Pol Capdevila <pcapdevila.pc@gmail.com>
2024-02-08 15:56:50 +0100

    Create SuppFigs.R

---
+++ Code/SuppFigs.R
@@ -0,0 +1,303 @@
+# --------------------------------------------------------------------------------------- #
+# - FILE NAME:   SupFigures.R         
+# - DATE:        15/09/2020
+# - DESCRIPTION: Code to produce the figures.
+# - AUTHORS:     Pol Capdevila Lanzaco (pcapdevila.pc@gmail.com)
+# --------------------------------------------------------------------------------------- #
+
+rm(list=ls(all=TRUE)) #remove everything
+
+library(ggplot2)
+library(tidybayes)
+library(tidyverse)
+library(dplyr)
+library(rstan)
+library(rstanarm)
+library(brms)
+library(bayesplot)
+library(bayestestR)
+library(ggdist)
+library(cowplot)
+library(tidyr)
+library(patchwork)
+library(MetBrewer)
+library(modelr)
+
+# Set default ggplot theme
+
+theme_set(theme_minimal()+
+            theme(axis.title.x = element_text(size=12,
+                                              margin = margin(t = 10, r = 0, b = 0, l = 0)), 
+                  axis.title.y = element_text(size=12,
+                                              margin = margin(t = 0, r = 10, b = 0, l = 0)),
+                  axis.line.x = element_line(color="black", linewidth = 0.5),
+                  axis.line.y = element_line(color="black", linewidth = 0.5),
+                  panel.grid.major = element_blank(), 
+                  panel.grid.minor = element_blank(),
+                  axis.text.x = element_text(color="black", size = 12),
+                  axis.text.y = element_text(color="black", size = 12),
+                  strip.text.x = element_text(size = 12),
+                  axis.ticks = element_line(color="black"),
+                  plot.title = element_text(hjust = 0.5)))
+
+# Set working directory
+
+path <- gsub("Code", "", dirname(rstudioapi::getActiveDocumentContext()$path))
+
+CodePath <- paste0(path,"Code")
+DataPath <-  paste0(path,"Data")
+ResultPath <-  paste0(path, "Results") 
+
+# Load data 
+load(paste0(ResultPath, "/models.RData"))
+load(paste0(DataPath,"/FinalData.RData"))
+
+# Table S1 ---------------------------------------------------------------------
+
+(TableS1 <- as.data.frame(describe_posterior(m1, ci = 0.95, test="none")) %>% 
+   mutate(Parameter = gsub("b_", "", Parameter),
+          Class = gsub("Class", "", Parameter),
+          Class =gsub(":", "", Class),
+          Class =gsub("System", "", Class),
+          Class =gsub("Freshwater", "", Class),
+          Class =gsub("Marine", "", Class),
+          Class =gsub("Terrestrial", "", Class),
+          Class = gsub('[[:digit:]]+', '', Class),
+          Class = gsub('Bonyfishes', 'Bony fishes', Class),
+          Class = gsub("D", "", Class), 
+          System = gsub("System", "", Parameter), 
+          System = gsub("Class", "", System), 
+          System = gsub("Mammalia", "", System), 
+          System = gsub("Aves", "", System), 
+          System = gsub("Bonyfishes", "", System), 
+          System = gsub("Reptilia", "", System), 
+          System = gsub("Elasmobranchii", "", System), 
+          System = gsub("Amphibia", "", System), 
+          System = gsub("D", "/", System), 
+          System =gsub("Class", "", System),
+          System =gsub(":", "", System),
+          System = gsub('[[:digit:]]+', '', System))  %>%
+   select(Class, System, Median, CI_low, CI_high, Rhat) %>% 
+   naniar::replace_with_na_all(condition= ~.x == "") %>% 
+   drop_na())
+
+# Save it 
+
+setwd(ResultPath)
+write.csv2(TableS1, "Table S1.csv")
+
+# Figure S5: Length ------------------------------------------------------------
+# Calculate mean trend
+
+mean <- pops_data %>%
+  data_grid(length=seq_range(length, n=101),
+            sigma=mean(sigma,na.rm = T)) %>%
+  distinct(ID, .keep_all = T) %>% 
+  add_epred_draws(mle, ndraws = 250,
+                  re_formula = NA) %>% 
+  group_by(length) %>% 
+  summarise(mean=mean(.epred))
+
+# Calculate individual draws
+
+(figs5 <- pops_data %>%
+    data_grid(length=seq_range(length, n=101),
+              sigma=mean(sigma,na.rm = T)) %>%
+    distinct(ID, .keep_all = T) %>% 
+    add_epred_draws(mle, ndraws = 250,
+                    re_formula = NA) %>%
+    ggplot(aes(x = length, y = .epred)) +
+    geom_hline(yintercept = 0, linetype="dashed", 
+               colour="grey25")+
+    geom_line(aes(group=.draw), colour="#A6323B", alpha = .2)+
+    geom_line(data = mean, aes(y=mean), linewidth=1)+
+    labs(x="Length of the time series",
+         y=expression(paste("Population trend (", mu, ")"))))
+
+# Save the figure
+
+ggsave("Figure S5.pdf", figs5,
+       width = 6, height = 4, path = ResultPath)
+
+# Table S2 ---------------------------------------------------------------------
+
+(TableS2 <- as.data.frame(describe_posterior(mle, ci = 0.95, test="none")) %>% 
+   mutate(Parameter = gsub("b_", "", Parameter),
+          Parameter = gsub("length", "Length of the time series", Parameter))  %>%
+   naniar::replace_with_na_all(condition= ~.x == "") %>%
+   drop_na() %>% 
+   select(Parameter, Median, CI_low, CI_high, Rhat)) 
+
+# Save it 
+
+setwd(ResultPath)
+write.csv2(TableS2, "Table S2.csv")
+
+# Figure S6: Decade --------------------------------------------------------
+
+# Plot
+
+(figs6 <- mdec %>% 
+   gather_draws(`b_.*`, regex = TRUE) %>%
+   median_qi(.value, .width = c(.95, .8, .5)) %>% 
+   mutate(.variable = gsub("b_", "", .variable),
+          .variable = gsub("decade", "", .variable))  %>%
+   naniar::replace_with_na_all(condition= ~.x == "") %>% 
+   drop_na() %>% 
+   filter(.variable!="Intercept") %>% 
+   ggplot(aes(y = .variable, x = .value)) +
+   geom_vline(xintercept = 0, linetype = "dashed", colour="grey50") +
+   geom_pointinterval(aes(xmin = .lower, 
+                          xmax = .upper),
+                      interval_size_range = c(0.5, 2)) +
+   scale_x_continuous(
+     labels = scales::number_format(accuracy = 0.01))+
+   labs(x="Posterior estimate", y = "") +
+   coord_flip()+
+   theme(legend.position = "none"))
+
+# Save the figure
+
+ggsave("Figure S6.pdf", figs6,
+       width = 6, height = 4, path = ResultPath)
+
+# Table S3 ---------------------------------------------------------------------
+
+(TableS3 <- as.data.frame(describe_posterior(mdec, ci = 0.95, test="none")) %>% 
+   mutate(Parameter = gsub("b_", "", Parameter),
+          Parameter = gsub("decade", "", Parameter))  %>%
+   naniar::replace_with_na_all(condition= ~.x == "") %>%
+   drop_na() %>% 
+   select(Parameter, Median, CI_low, CI_high, Rhat) %>% 
+   arrange(desc(Parameter))) 
+
+# Save it 
+
+setwd(ResultPath)
+write.csv2(TableS3, "Table S3.csv")
+
+# Figure S7: Protection --------------------------------------------------------
+# Sample size
+
+sample_size <- pops_data %>% 
+  drop_na(Protected) %>%
+  mutate(Protected = gsub("Yes", "Protected", Protected),
+         Protected =gsub("No", "Unprotected", Protected)) %>% 
+  group_by(Protected) %>% 
+  summarise(median=median(mu),
+            n=n(),
+            max=max(mu))
+
+med <- mp %>%
+  gather_draws(`b_.*`, regex = TRUE) %>%
+  mutate(Protected = gsub("b_", "", .variable),
+         Protected = gsub("Yes", "", Protected),
+         Protected =gsub("ProtectedNo", "Unprotected", Protected))  %>%
+  group_by(Protected) %>% 
+  summarise(med=quantile(.value, probs = 0.9)) %>% 
+  naniar::replace_with_na_all(condition= ~.x == "") %>% 
+  drop_na() 
+
+sample_size<- sample_size %>% 
+  left_join(med)
+
+# Plot intervals 
+
+dat <- mp %>%
+  gather_draws(`b_.*`, regex = TRUE) %>%
+  mutate(Protected = gsub("b_", "", .variable),
+         Protected = gsub("Yes", "", Protected),
+         Protected =gsub("ProtectedNo", "Unprotected", Protected))  %>%
+  naniar::replace_with_na_all(condition= ~.x == "") %>% 
+  drop_na()
+
+# Plot
+
+(figs7 <- mp %>%
+    gather_draws(`b_.*`, regex = TRUE) %>%
+    median_qi(.value, .width = c(.95, .8, .5)) %>% 
+    mutate(Protected = gsub("b_", "", .variable),
+           Protected = gsub("Yes", "", Protected),
+           Protected =gsub("ProtectedNo", "Unprotected", Protected))  %>%
+    naniar::replace_with_na_all(condition= ~.x == "") %>% 
+    drop_na() %>% 
+    ggplot(aes(y = Protected, x = .value)) +
+    geom_vline(xintercept = 0, linetype = "dashed", colour="grey50") +
+    geom_pointinterval(aes(xmin = .lower, 
+                           xmax = .upper,
+                           colour=Protected),
+                       interval_size_range = c(0.5, 2)) +
+    geom_text(data = sample_size, aes(x=med, y=Protected, 
+                                      label = paste0("n=", n)),
+              vjust   = -1)+
+    scale_color_manual("", values = c("#A1D6E2","#336B87")) +
+    scale_x_continuous(
+      labels = scales::number_format(accuracy = 0.01))+
+    xlim(-0.025,0.025)+
+    labs(x="Posterior estimate", y = "") + 
+    theme(legend.position = "none"))
+
+# Save the figure
+
+ggsave("Figure S7.pdf", figs7,
+       width = 6, height = 4, path = ResultPath)
+
+# Table S4 ----------------------------------------------------------------------
+
+(TableS4 <- as.data.frame(describe_posterior(mp, ci = 0.95, test="none")) %>% 
+   mutate(Parameter = gsub("b_", "", Parameter),
+          Parameter = gsub("Yes", "", Parameter),
+          Parameter =gsub("ProtectedNo", 
+                          "Unprotected", Parameter))  %>%
+   naniar::replace_with_na_all(condition= ~.x == "") %>%
+   drop_na() %>% 
+   select(Parameter, Median, CI_low, CI_high, Rhat)) 
+
+# Save it 
+
+setwd(ResultPath)
+write.csv2(TableS4, "Table S4.csv")
+
+# Table S5 ---------------------------------------------------------------------
+
+(TableS5 <- as.data.frame(describe_posterior(md, ci = 0.95, test="none")) %>%
+   mutate(Model="Database") %>% 
+   rbind(as.data.frame(describe_posterior(mc2, ci = 0.95, test="none")) %>% 
+           mutate(Model="IUCN")) %>% 
+   rbind(as.data.frame(describe_posterior(ml, ci = 0.95, test="none")) %>% 
+           mutate(Model="Data source")) %>% 
+   mutate(Parameter = gsub("b_threat", "", Parameter),
+          Parameter = gsub("b_", "", Parameter),
+          Parameter = gsub("database", "", Parameter),
+          Parameter = gsub("Data_source_type", "", Parameter),
+          Parameter = gsub("IV", "IbeV", Parameter),
+          Parameter = gsub("M", "-", Parameter),
+          Parameter = gsub("Grey", "Grey ", Parameter))  %>%
+   select(Model, Parameter, Median, CI_low, CI_high, Rhat) %>% 
+   naniar::replace_with_na_all(condition= ~.x == "") %>% 
+   drop_na())
+
+# Save it 
+
+setwd(ResultPath)
+write.csv2(TableS5, "Table S5.csv")
+
+# Table S6 ---------------------------------------------------------------------
+
+(TableS6 <- as.data.frame(describe_posterior(mdi, ci = 0.95, test="none")) %>%  
+   mutate(Parameter = gsub("b_", "", Parameter),
+          Parameter = gsub("database", "", Parameter),
+          Parameter = gsub("Data_source_type", "", Parameter),
+          Parameter = gsub("IV", "IbeV", Parameter),
+          Parameter = gsub("System", "", Parameter),
+          Parameter = gsub("MarineD", "Marine/", Parameter),
+          Parameter = gsub("FreshwaterD", "Freshwater/", Parameter),
+          Parameter = gsub("Class", "", Parameter))  %>%
+   select(Parameter, Median, CI_low, CI_high, Rhat) %>% 
+   naniar::replace_with_na_all(condition= ~.x == "") %>% 
+   drop_na())
+
+# Save it 
+
+setwd(ResultPath)
+write.csv2(TableS6, "Table S6.csv")
